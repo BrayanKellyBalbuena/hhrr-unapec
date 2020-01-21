794183e37409ababf82e1a864c3383ead7d54568
BrayanKellyBalbuena <the.script.bryan07@gmail.com>
2020-01-21 17:57:26 -0400

    Done initial project structure

---
+++ src/api/src/main/java/edu/unapec/hhrr/HhrrApplication.java
@@ -0,0 +1,13 @@
+package edu.unapec.hhrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhrrApplication.class, args);
+	}
+
+}
